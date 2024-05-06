13ea0932a1eec82d1c4f50f1bf7adaa1f0b31f90
EnelSkypieaRuler <bilelyaa456@gmail.com>
2024-05-06 22:37:48 +0100

    added code owners, start and stop scripts

---
+++ scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Web challenges
+echo "Starting web challenges"
+docker compose up -f '../challenges/web/shopify/compose.yml' -d
+
+
+
+# Misc challenges
+echo "Starting misc challenges"
